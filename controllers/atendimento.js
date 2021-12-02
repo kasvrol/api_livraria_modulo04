48a0a9a9007c802a66bdf7b55d8341b80f2998c5
kasvrol <dovalekarolina@gmail.com>
2021-12-02 20:55:37 -0300

    abertura da rota POST

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,3 +1,4 @@
 module.exports = app => {
     app.get('/', (req, res)=> res.send('servidor rodando na rota GET, tudo beleza'))
+    app.post('/', (req, res)=> res.send('servidor rodando na rota POST, tudo beleza'))
 }
