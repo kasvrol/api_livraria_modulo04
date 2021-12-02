28c9716c288bc4e445059a25aad2439d3493ef38
kasvrol <dovalekarolina@gmail.com>
2021-12-02 20:27:04 -0300

    crianção da pasta controllers

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
-app.get('/', (req, res)=> res.send('servidor rodando na rota GET'))
+app.get('/', (req, res)=> res.send('servidor rodando na rota GET, tudo beleza'))
