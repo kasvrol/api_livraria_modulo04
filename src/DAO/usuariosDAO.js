6dbdb7e1df497bd11cabc8f5a4e9cb7918efdced
kasvrol <dovalekarolina@gmail.com>
2021-12-03 19:56:46 -0300

    mudança no select de usuarios do arquivo usuariosDAO.js

--- src/DAO/usuariosDAO.js
+++ src/DAO/usuariosDAO.js
@@ -1,116 +1,90 @@
-const usuariosENovosUsuarios = class usuariosDAO {
-    constructor(db) {
-        this.db = db;
-    }
+class usuariosENovosUsuarios {
 
-    getAllUsuarios(){
-        const tab = 'SELECT * FROM USUARIOS'
-        return new Promise ((resolve, reject) => {
-            this.db.all (tab, (error, rows) => {
-                if (error) {
-                    reject ({
-                        "msg": error.message,
-                        "error": true
-                    })
-                } else {
-                    resolve ({
-                        "usuarios": rows,
-                        "count": rows.length,
-                        "error": false
-                    })
-                }
-            }) 
-        })
-    }
-
-    postUsuario(newUsuario) {
-        const tab = 'INSERT INTO USUARIO (id_usuario, cpf, primeiro_nome, sobrenome, email, senha, CEP, endereco) VALUES (?,?,?,?,?,?,?,?)'
-        const post = [newUsuario.id_usuario, newUsuario.cpf, newUsuario.primeiro_nome, newUsuario.sobrenome, newUsuario.email, newUsuario.senha, newUsuario.cep, newUsuario.endereco]
-        
-        return new Promise((resolve, reject)=> {
-            this.db.run(tab, post, (error) => {
-                if (error) {
-                    reject ({
-                        "msg": error.message,
-                        "error": true
-                    })
-                } else {
-                    resolve ({
-                        "req": newUsuario,
-                        "error": false
-                    })
-                }
+    selectUsuario(usuariosdb) {
+      return new Promise((resolve, reject) => {
+        usuariosdb.all('SELECT * FROM USUARIO', (err, rows) => {
+          if (err) {
+            reject(({ "mensagem": err.message, 
+                        "error": true }))
+          } else {
+            resolve({
+              "usuarios": rows,
+              "error": false
             })
+          }
         })
-    }
-
-    getIdUsuario(id){
-        const tab = `SELECT * FROM USUARIO WHERE ID = ?`
-        return new Promise((resolve, reject)=> {
-            this.db.all(tab, id, (error, rows)=> {
-                if (error) {
-                    reject ({
-                        "msg": error.message,
-                        "error": true
-                    })
-                } else {
-                    resolve ({
-                        "req": rows,
-                        "error": false
-                    })
-                }
-            })
-        })
-    }
-
-    async updateUsuario(id, newUsuario) {
-        try {
-            const tab = `UPDATE USUARIO SET ID_USUARIO = (?), CPF = (?), PRIMEIRO_NOME = (?), SOBRENOME = (?), EMAIL = (?), SENHA = (?), CEP = (?), ENDERECO = (?), EMAIL = (?)`
-            const update = [newUsuario.id_usuario, newUsuario.cpf, newUsuario.primeiro_nome, newUsuario.sobrenome, newUsuario.email, newUsuario.senha, newUsuario.cep, newUsuario.endereco]
-            return new Promise((resolve, reject)=> {
-                this.db.run(tab, update, (error)=> {
-                    if (error) {
-                        reject (error)
-                    } else {
-                        resolve ({
-                            "msg": `O usuario com id ${id} foi atualizado`,
-                            "cliente": newUsuario,
-                            "error": false
-                        })
-                    }
-                })
-            })
-        }
-        catch (error) {
-            throw new Error(error.message)
-        }
-    }
-
-   async deleteUsuarios(id) {
-        try {
-            const usuarios = await this.getIdUsuarios(id)
-            if (usuarios.req.length) {
-                const tab = `DELETE FROM USUARIOS WHERE ID = ?`
-
-                return new Promise((resolve, reject)=> {
-                    this.db.run(tab, id, (error) => {
-                        if (error) {
-                            reject (error.message)  
-                        } else {
-                            resolve ({
-                                "msg": `O usuario com id ${id} foi deletado`,
-                                "error": false
-                            })
-                        }
-                    })
-                })
-            } else {
-                throw new Error (`Usuario com id ${id} não existe`)
-        }
-    } catch (error) {
-            throw new Error(error.message)
       }
+      )
     }
-} 
-
-module.exports = usuariosENovosUsuarios
+  
+    selectID(id, usuariosdb) {
+      return new Promise((resolve, reject) => {
+        usuariosdb.get('SELECT * FROM USUARIO WHERE id_usuario = ?', id, (err, rows) => {
+          if (err) {
+            reject(({ "mensagem": err.message,
+                        "error": true }))
+          } else {
+            resolve({
+              "id_usuario": rows,
+              "error": false
+            })
+          }
+        })
+      }
+      )
+    }
+  
+  
+    adicionaUsuario(body, usuariosdb) {
+      return new Promise((resolve, reject) => {
+        usuariosdb.run(`INSERT INTO USUARIO (id_usuario, cpf, primeiro_nome, sobrenome, email, senha, cep, endereco) VALUES (?, ?, ?, ?, ?, ?, ?)`, [body.id_usuario, body.cpf, body.primeiro_nome, body.sobrenome, body.email, body.senha, body.cep, body.endereco], (err, rows) => {
+          if (err) {
+            reject(({ "mensagem": err.message, "error": true }))
+          } else {
+            resolve({
+              "usuario": body,
+              "error": false
+            })
+          }
+        })
+      }
+      )
+    }
+  
+  
+  
+    atualUsuario(id, body, usuariosdb) {
+      return new Promise((resolve, reject) => {
+        usuariosdb.run(`UPDATE USUARIO SET (id_usuario, cpf, primeiro_nome, sobrenome, email, senha, cep, endereco) = (?, ?, ?, ?, ?, ?, ? WHERE id_usuario = ?`, [body.id_usuario, body.cpf, body.primeiro_nome, body.sobrenome, body.email, body.senha, body.cep, body.endereco], (err, rows) => {
+          if (err) {
+            reject(({ "mensagem": err.message, "error": true }))
+          } else {
+            resolve({
+              "usuario": id,
+              "error": false
+            })
+          }
+        })
+      }
+      )
+    }
+  
+  
+    deleteUsuario(id, usuariosdb) {
+      return new Promise((resolve, reject) => {
+        usuariosdb.run(`DELETE FROM USUARIO WHERE id_usuario = ?`, id, (err, rows) => {
+          if (err) {
+            reject(({ "mensagem": err.message, "error": true }))
+          } else {
+            resolve({
+              "usuario": id,
+              "error": false
+            })
+          }
+        })
+      }
+      )
+    }
+  }
+  
+  module.exports = new usuariosENovosUsuarios
