ce7173d61a6bc28684ff9d6c0bf02d8dded4140f
kasvrol <dovalekarolina@gmail.com>
2021-12-03 19:02:20 -0300

    inserindo rotas patch, delete e get:id no arquivo usuarios-controller.js

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -4,3 +4,32 @@ module.exports = app => {
         console.log(req.body)
         res.send('servidor rodando na rota POST, tudo beleza')})
 }
+const modeloUsuarios = require('../models/modelo_usuarios')
+
+const usuarios = (app) => {
+    app.get('/', (req, res) => {
+      res.send('<h2>API rodando na rota GET</h2>')
+    })
+  
+    app.get('/usuarios', (req, res) => { 
+        modeloUsuarios.mostraUsuarios(req, res)
+    })
+  
+    app.get('/usuarios/:id', (req, res) => { 
+        modeloUsuarios.selecionaIDUsuario(req, res)
+    })
+  
+    app.post('/usuarios', (req, res) => {
+        modeloUsuarios.criaUsuario(req, res)
+    })
+  
+    app.patch('/usuarios/:id', (req, res) => {
+        modeloUsuarios.atualUsuario(req, res)
+    })
+  
+    app.delete('/usuarios/:id', (req, res) => {
+        modeloUsuarios.deletaUsuario(req, res)
+    })
+  }
+  
+  module.exports = usuarios
