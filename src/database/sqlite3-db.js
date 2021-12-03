54fe4e1134eb531af9b9e77d6efcccedb0e7c793
kasvrol <dovalekarolina@gmail.com>
2021-12-03 19:35:45 -0300

    comentando o código do arquivo sqlite3

--- src/database/sqlite3-db.js
+++ src/database/sqlite3-db.js
@@ -1,11 +1,15 @@
+// solicitação do sqlite3
 const sqlite3 = require('sqlite3').verbose();
+// solicitação do path
 const path = require('path');
+// solicitação do caminho da arquitetura
 const caminhoArq = path.resolve(__dirname,'database.db')
+// solicitação do bd
 const bd = new sqlite3.Database(caminhoArq);
 process.on('SIGINT', () =>
     bd.close(() => {
         process.exit(0);
     })
 );
-
+// exportação do bd
 module.exports = bd;
