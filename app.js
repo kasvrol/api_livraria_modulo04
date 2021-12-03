2c339e35bd001983721e880b2a46e8bfb6f78e6f
kasvrol <dovalekarolina@gmail.com>
2021-12-03 16:35:23 -0300

    inserindo configuração de porta e solicitando controllers

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+// solicitando o arquivo customExpress.js
 const customExpress = require('./src/config/customExpress.js')
+// configurando app e porta
 const app = customExpress()
+const PORT = process.env.PORT || 3000
+// solicitando o arquivo usuarios-controller.js
+const usuarioController = require('./src/controllers/usuario-controller.js')
+usuarioController(app)
+
 const usuarios = require('./src/database/usuarios.js')
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+app.listen(PORT, () => console.log('servidor rodando na porta 3000'))
+///////////////////////////////////////////////////
+
