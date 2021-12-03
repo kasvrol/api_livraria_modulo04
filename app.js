4d016878378950758e9b3993e850ad3495485368
kasvrol <dovalekarolina@gmail.com>
2021-12-02 23:13:19 -0300

    adicionando a const usuarios no app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const customExpress = require('./src/config/customExpress.js')
 const app = customExpress()
+const usuarios = require('./src/database/usuarios.js')
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
