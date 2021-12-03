1cd32b7ddffc61e21c6380701ae8b13ecc2cee26
kasvrol <dovalekarolina@gmail.com>
2021-12-02 21:02:18 -0300

    requisição.body

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,4 +1,6 @@
 module.exports = app => {
     app.get('/', (req, res)=> res.send('servidor rodando na rota GET, tudo beleza'))
-    app.post('/', (req, res)=> res.send('servidor rodando na rota POST, tudo beleza'))
+    app.post('/', (req, res)=> {
+        console.log(req.body)
+        res.send('servidor rodando na rota POST, tudo beleza')})
 }
