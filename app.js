8a5d02f19d6b42862096bdc29f255c18b080a1e8
kasvrol <dovalekarolina@gmail.com>
2021-12-07 20:03:59 -0300

    alteração no código de requisição do app.js

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const app = customExpress()
 const PORT = process.env.PORT || 3000
 // solicitando o arquivo usuarios-controller.js
 const usuarioController = require('./src/controllers/usuario-controller.js')
-usuarioController(app)
+const db = require('./database/sqlite3-db')
+usuarioController(app, db)
 
 const usuarios = require('./src/database/criar-dados.js')
-app.listen(PORT, () => console.log('servidor rodando na porta 3000'))
-///////////////////////////////////////////////////
-
+app.listen(PORT, () => console.log(`servidor rodando na porta ${PORT}`))
