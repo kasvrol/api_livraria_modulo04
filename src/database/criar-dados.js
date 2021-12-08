5e13a4a71ee2376fbfb8050cbb8f8b3224b71b3e
kasvrol <dovalekarolina@gmail.com>
2021-12-08 14:20:58 -0300

    criação de um novo database e correção de bug no arquivo criar-dados.js

--- src/database/criar-dados.js
+++ src/database/criar-dados.js
@@ -13,14 +13,14 @@ CREATE TABLE IF NOT EXISTS "USUARIO" (
     "senha" varchar(6),
     "cep" varchar(8),
     "endereco" varchar(255)
-);
+)
 `;
 
 const usuarioInserido = `INSERT INTO USUARIO (id_usuario, cpf, primeiro_nome, sobrenome, email, senha, cep, endereco)
         VALUES
-        (0, '00000000000', 'Janio', 'Quadros', 'janioquadros@gmail.com', 'querosair', '1111-111', 'republica do brasil, 190')
-        (1, '00000000001', 'João', 'Quadros', 'joaoquadros@gmail.com', 'queroentrar', '1111-111', 'republica do brasil, 191')
-        (2, '00000000002', 'Juarez', 'Quadros', 'juarezquadros@gmail.com', 'queroficar', '1111-111', 'republica do brasil, 192')
+        (0, '00000000000', 'Janio', 'Quadros', 'janioquadros@gmail.com', 'querosair', '1111-111', 'republica do brasil, 190'),
+        (1, '00000000001', 'João', 'Quadros', 'joaoquadros@gmail.com', 'queroentrar', '1111-111', 'republica do brasil, 191'),
+        (2, '00000000002', 'Juarez', 'Quadros', 'juarezquadros@gmail.com', 'queroficar', '1111-111', 'republica do brasil, 192'),
         (3, '00000000003', 'Jacinto', 'Quadros', 'jacintoquadros@gmail.com', 'naosei', '1111-111', 'republica do brasil, 193')       
 `;
 
